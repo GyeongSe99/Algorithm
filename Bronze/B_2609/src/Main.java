e984adec521fb80726992ce22749b0e3a91c5ed8
천세경 <jay45652@gmail.com>
2024-02-06 09:07:06 +0900

    백준 2609 : 최대 공약수, 최소 공배수 구하기

---
+++ Bronze/B_2609/src/Main.java
@@ -0,0 +1,29 @@
+// 백준 2609 : 최대공약수, 최소공배수 구하기
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = gcd(a, b);
+        int lcm = lcm(a, b);
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        return (a / gcd) * (b / gcd) * gcd;
+    }
+}
