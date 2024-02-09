0876c111fa2b194c74ebc1e765dfd2b6db02567e
천세경 <sgcheon@naver.com>
2024-02-10 06:44:05 +0900

    백준 1037 : 약수

---
+++ Bronze/B_1037/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(arr);
+
+        long answer;
+
+        if (arr.length % 2 == 0) {
+            answer = (long) arr[N / 2 - 1] * arr[N / 2];
+        } else {
+            answer = (long) arr[N / 2] * arr[N / 2];
+        }
+
+        System.out.println(answer);
+
+    }
+
+
+}
