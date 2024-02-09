f0a325ce77fdb9254286f1572b0a719049178e92
천세경 <sgcheon@naver.com>
2024-02-10 06:09:32 +0900

    백준 2702 : 초6 수학

---
+++ Bronze/B_2702/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int gcd = gcd(a, b);
+            int lcm = lcm(a, b);
+
+            System.out.println(lcm + " " + gcd);
+        }
+
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        return (a / gcd) * (b / gcd) * gcd;
+    }
+}
