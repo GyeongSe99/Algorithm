0f56af5bc923063069b1168bd057149fa7ec4ff7
천세경 <sgcheon@naver.com>
2024-02-10 19:24:49 +0900

    백준 17206 : 준석이의 수학 숙제

---
+++ Bronze/B_17206/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();
+
+        int[] test = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int num : test) {
+            int last3 = num / 3;
+            int last7 = num / 7;
+            int last21 = num / 21;
+            int sum = (3 * last3 * (last3 + 1) / 2) + (7 * last7 * (last7 + 1) / 2) - (21 * last21 * (last21 + 1) / 2);
+
+            System.out.println(sum);
+        }
+
+    }
+}
