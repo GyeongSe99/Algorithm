fa6e070993e21df1b8ca6298b9653ac06a6744b1
GyeongSe99 <jay45652@gmail.com>
2024-02-13 16:43:26 +0900

    백준 9012 : 괄호

---
+++ Silver/S_9012/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            Stack<Character> stack = new Stack<>();
+            String s = br.readLine();
+
+            boolean isValid = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+
+                if (c == '(') {
+                    stack.push(c);
+                } else {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    } else {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!stack.isEmpty()) {
+                isValid = false;
+            }
+
+            if (isValid) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+}
