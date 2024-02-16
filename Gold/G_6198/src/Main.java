ca02b2b88d5b699658a99d35ddfb3f23a1495c98
GyeongSe99 <jay45652@gmail.com>
2024-02-17 05:14:10 +0900

    백준 6198 : 옥상 정원 꾸미기

--- Gold/G_6198/src/Main.java
+++ Gold/G_6198/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,7 +13,30 @@ public class Main {
             list[i] = n;
         }
 
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        
+        long sum = 0;
 
+        for (int i = 1; i < list.length; i++) {
+            int now = list[i];
 
+            while (!stack.isEmpty()) {
+                if (list[stack.peek()] <= now) {
+                    sum += Math.abs(i - 1 - stack.pop());
+                } else {
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        int lastIdx = list.length;
+        while (!stack.isEmpty()) {
+            sum += Math.abs(lastIdx - 1 - stack.pop());
+        }
+
+        System.out.println(sum);
     }
 }
