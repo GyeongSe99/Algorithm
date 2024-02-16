396c95b7bfbff392bc4b004bf2aa03aa1fbe307d
GyeongSe99 <jay45652@gmail.com>
2024-02-16 17:52:05 +0900

    백준 6198 : 옥상 정원 꾸미기 미완성

--- Gold/G_6198/src/Main.java
+++ Gold/G_6198/src/Main.java
@@ -2,8 +2,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] list = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            list[i] = n;
+        }
+
+
 
     }
 }
