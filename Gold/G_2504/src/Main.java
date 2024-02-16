4dfbf3a5bca305085a8258417111c84a76493f7f
GyeongSe99 <jay45652@gmail.com>
2024-02-16 09:44:01 +0900

    백준 2504 : 괄호의 값

--- Gold/G_2504/src/Main.java
+++ Gold/G_2504/src/Main.java
@@ -1,95 +1,89 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    static Stack<String> stack;
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        Stack<Character> stack = new Stack<>();
+        String[] s = sc.nextLine().split("");
+        stack = new Stack<>();
 
         boolean isValid = true;
 
-        if (s.length() == 1) {
-            isValid = false;
-        }
+        for (int i = 0; i < s.length; i++) {
+            String str = s[i];
 
-        int result = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (!isValid) break;
-
-            char c = s.charAt(i);
-
-            if ( c == '(') {
-                stack.push('+');
-                stack.push('(');
-            } else if (c == '[') {
-                stack.push('+');
-                stack.push('[');
-            } else if (c == ')') {
-                int cal = 1;
-                int temp = 0;
-
-                while (!stack.isEmpty()) {
-                    char now = stack.pop();
-                    if (now == '(') {
-                        cal *= 2;
-                        stack.push((char)(cal + '0'));
-                        break;
-                    } else if (now == '+') {
-                        if (cal == 1) {
-                            cal += temp - 1;
-                        } else {
-                            cal += temp;
-                        }
-                        temp = 0;
-                    } else if (now == '[') {
-                        isValid = false;
-                        break;
-                    } else {
-                        temp += now - '0';
-                    }
-                }
+            if ( str.equals("(") || str.equals("[")) {
+                stack.push(str);
             } else {
-                int cal = 1;
-                int temp = 0;
+                if (stack.isEmpty()) {
+                    isValid = false;
+                    break;
+                }
 
-                while (!stack.isEmpty()) {
-                    char now = stack.pop();
-                    if (now == '[') {
-                        cal *= 3;
-                        stack.push((char)(cal + '0'));
-                        break;
-                    } else if (now == '+') {
-                        if (cal == 1) {
-                            cal += temp - 1;
-                        } else {
-                            cal += temp;
-                        }
-                    } else if (now == '(') {
+                if (str.equals(")")) {
+                    if (calculateInBracket(")", "(", "[") == -1) {
                         isValid = false;
-                    } else {
-                        temp += now - '0';
+                        break;
+                    }
+                } else {    // "]"
+                    if (calculateInBracket("]", "[", "(") == -1) {
+                        isValid = false;
+                        break;
                     }
                 }
             }
         }
 
-        while (!stack.isEmpty() && isValid){
-            char c = stack.pop();
-            if (c == '[' || c == '(' || c == ')' || c == ']') {
+        int sum = 0;
+        while (!stack.isEmpty() && isValid) {
+            String str = stack.pop();
+            if (str.equals("[") || str.equals("(") || str.equals(")") || str.equals("]")) {
                 isValid = false;
+                break;
             } else {
-                if (c != '+') {
-                    int num = c - '0';
-                    result += num;
-                }
+                sum += Integer.parseInt(str);
             }
         }
 
         if (isValid) {
-            System.out.println(result);
+            System.out.println(sum);
         } else {
             System.out.println(0);
         }
+    }
+
+    // return = -1 -> 잘못된 괄호
+    // str = 현재 들어온 닫는 괄호
+    // pair = 현재 들어온 닫는 괄호와 짝을 이루는 여는 괄호
+    // notPair = 현재 들어온 닫는 괄호와 짝을 이루지 않는 괄호
+    static int calculateInBracket(String str, String pair, String notPair) {
+        if (!stack.contains(pair)) {
+            return -1;
+        }
+
+        int result = 0;
+        int value = str.equals(")") ? 2 : 3;
+
+        if (stack.peek().equals(pair)) {
+            stack.pop();
+            stack.push(String.valueOf(value));
+            return value;
+        } else {
+            while (!stack.isEmpty()) {
+                String now = stack.pop();
+                if (now.equals(notPair)) {
+                    return -1;
+                } else if (now.equals(pair)){
+                    stack.push(String.valueOf(result * value));
+                    return result;
+                } else {
+                    result += Integer.parseInt(now);
+                }
+            }
+        }
+
+        return -1;
     }
 }
