503e34892496f37a9a1c4579a048ed8f8930ae03
GyeongSe99 <jay45652@gmail.com>
2024-02-24 12:11:15 +0900

    백준 11726 : 신입사원

---
+++ Silver/S_11726/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int mod = 10007;
+
+        int[] dp = new int[n + 2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] % mod + dp[i - 2] % mod) % mod;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
