b714b6eed98f2df6476ce203eb04d9acecfd207f
천세경 <jay45652@gmail.com>
2024-03-28 18:00:23 +0900

    SW academy : 하나로

---
+++ SWacademy/src/hanaro/Solution.java
@@ -0,0 +1,23 @@
+package hanaro;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    static int[][] islands;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());    // 섬의 개수
+
+            islands = new int[N][N];
+            for (int i = 0; i < N; i++) {
+
+            }
+        }
+
+    }
+}
