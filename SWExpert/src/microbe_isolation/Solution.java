b4f7479d42f1c9080324975fc187316380f86d3d
천세경 <jay45652@gmail.com>
2024-04-08 21:18:53 +0900

    SW Academy : 미생물 격리(실패)

--- SWExpert/src/microbe_isolation/Solution.java
+++ SWExpert/src/microbe_isolation/Solution.java
@@ -6,8 +6,7 @@ package microbe_isolation;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
     static class Micro {
@@ -18,6 +17,20 @@ public class Solution {
             this.c = c;
             this.d = d;
             this.capacity = capacity;
+        }
+
+        public Micro(Micro micro) {
+            this(micro.r, micro.c, micro.d, micro.capacity);
+        }
+
+        @Override
+        public String toString() {
+            return "Micro{" +
+                    "r=" + r +
+                    ", c=" + c +
+                    ", d=" + d +
+                    ", capacity=" + capacity +
+                    '}';
         }
     }
 
@@ -30,11 +43,12 @@ public class Solution {
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
-        for (int test_case = 0; test_case < T; test_case++) {
+        for (int test_case = 1; test_case < T; test_case++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
+            micros = new ArrayList<>();
 
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -49,27 +63,95 @@ public class Solution {
             for (int i = 0; i < M; i++) {
                 move();
                 merge();
+
             }
+
+            int sum = 0;
+            for (int i = 0; i < micros.size(); i++) {
+                sum += micros.get(i).capacity;
+            }
+
+            System.out.println("#" + test_case + " " + sum);
         }
     }
 
     // 이동하는 메소드
     private static void move() {
+        for (int i = 0; i < micros.size(); i++) {
+            Micro micro = micros.get(i);
+            micro.r = micro.r + d[micro.d][0];
+            micro.c = micro.c + d[micro.d][1];
 
+            if (checkIsBorder(micro)) {
+                micro.capacity = micro.capacity / 2;
+                if (micro.capacity != 0) {
+                    changeDirection(micro);
+                }
+            }
+        }
+
+        micros.removeIf(micro -> micro.capacity == 0);
     }
 
     // 같은 자리 겹치는 애들 합치는 메소드
     private static void merge() {
+        List<Micro> copy = new ArrayList<>();
+        for (Micro micro : micros) {
+            copy.add(new Micro(micro));
+        }
 
+        int cur = 0;
+
+        while (cur < copy.size()) {
+            if (copy.size() <= 1) {
+                break;
+            }
+            Micro curMic = copy.get(cur);
+
+            int maxIdx = cur;   // 제일 큰 애의 위치
+            int maxCap = curMic.capacity;
+            int sum = curMic.capacity;
+            int size = copy.size();
+
+            for (int i = cur + 1; i < size; i++) {
+                Micro m = copy.get(i);
+                // 같은 위치에 있으면
+                if (curMic.r == m.r && curMic.c == m.c) {
+                    // 일단 값 합쳐주고
+                    sum += m.capacity;
+                    // 값이 더 크면 갱신
+                    if (maxCap < m.capacity) {
+                        maxCap = m.capacity;
+                        maxIdx = i;
+                    }
+                    m.capacity = 0;
+                }
+            }
+
+            curMic.d = copy.get(maxIdx).d;
+            curMic.capacity = sum;
+            cur++;
+        }
+
+        copy.removeIf(micro -> micro.capacity == 0);
+        micros = copy;
     }
 
     // 끝자락에 있는지 확인하기
     private static boolean checkIsBorder(Micro micro) {
-        return false;
+        return micro.r == 0 || micro.c == 0 || micro.r == N - 1 || micro.c == N - 1;
     }
 
     // 방향 바꿔주기
     private static void changeDirection(Micro micro) {
-
+         if (micro.d == 0) {
+             micro.d = 1;
+         } else if (micro.d == 1) {
+             micro.d = 0;
+         } else if (micro.d == 2) {
+             micro.d = 3;
+         } else {
+             micro.d = 2;
+         }
     }
 }
