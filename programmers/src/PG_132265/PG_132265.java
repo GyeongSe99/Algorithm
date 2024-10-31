c3f9361e47cfa41a5b799646211aa0effc8abe79
천세경 <jay45652@gmail.com>
2024-10-31 19:14:38 +0900

    24-10-27

---
+++ programmers/src/PG_132265/PG_132265.java
@@ -0,0 +1,10 @@
+package PG_132265;
+// 토핑들의 종류에 더 관심이 많습니다
+// 그래서 잘린 조각들의 크기와 올려진 토핑의 개수에 상관없이 각 조각에 동일한 가짓수의 토핑이 올라가면 공평하게 롤케이크가 나누어진 것으로 생각
+
+public class PG_132265 {
+    public int solution(int[] topping) {
+        int answer = -1;
+        return answer;
+    }
+}
