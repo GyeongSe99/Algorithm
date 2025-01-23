195ec62fe1254acadeccf5c77681dbba6a540ae8
천세경 <jay45652@gmail.com>
2025-01-23 21:34:41 +0900

    2025-01-23 백준 : 집합

---
+++ Silver/src/S_11723.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class S_11723 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<Integer> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            int num;
+
+            switch (cmd) {
+                case "add":
+                    num = Integer.parseInt(st.nextToken());
+                    set.add(num);
+                    break;
+                case "remove":
+                    num = Integer.parseInt(st.nextToken());
+                    set.remove(num);
+                    break;
+                case "check":
+                    num = Integer.parseInt(st.nextToken());
+                    if (set.contains(num)) {
+                        sb.append("1").append("\n");
+                    } else {
+                        sb.append("0").append("\n");
+                    }
+                    break;
+                case "toggle":
+                    num = Integer.parseInt(st.nextToken());
+                    if (set.contains(num)) {
+                        set.remove(num);
+                    } else {
+                        set.add(num);
+                    }
+                    break;
+                case "all":
+                    for (int j = 1; j <= 20; j++) {
+                        set.add(j);
+                    }
+                    break;
+                case "empty":
+                    set.clear();
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
