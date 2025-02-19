faf72784ae9ca4fe8c951194e3b6d94cf68202e0
천세경 <jay45652@gmail.com>
2025-02-19 15:36:53 +0900

    2025-02-18 백준: 별찍기-10

---
+++ Gold/src/G_2447.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class G_2447 {
+    private static char[][] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = ' ';
+            }
+        }
+
+        star(0, 0, N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void star(int x, int y, int size) {
+        if (size  == 1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) continue;
+                star(x + i * (size / 3), y + j * (size/3), size/3);
+            }
+        }
+    }
+}
